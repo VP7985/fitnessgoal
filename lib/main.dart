28fb98a78020ca75bbf130b6b2e8d68e9a227178
Abhisheksharma654 <Abhisheksharma654@gmail.com>
2024-05-01 21:40:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fitnessgoal/firebase_options.dart';
+import 'package:fitnessgoal/loginpage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Goalify',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Loginpage(),
+    );
+  }
+}
